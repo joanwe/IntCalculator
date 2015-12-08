21dc9277050ca6afddb07d357e52640e5ecd4351
卢乔志 <joanwe@imac.local>
2015-12-09 02:17:58 +0800

    Calculator v1.0.0

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,87 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var clear = true
+    var sum = 0
+    var key = 0 //1加2减3乘4除
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func Number(sender: UIButton) {
+        sender.showsTouchWhenHighlighted = true
+        if clear {
+            Display.text = sender.currentTitle
+            clear = false
+        }else{
+            Display.text = Display.text! + sender.currentTitle!
+            if Display.text == "00" {
+                Display.text = "0"
+                clear = true
+            }
+            
+        }
+        
+           }
+    
+    @IBAction func Equal() {
+        Method()
+        key = 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    @IBOutlet weak var Display: UILabel!
+    @IBOutlet weak var Reset: UIButton!
+    
+    @IBOutlet weak var Acc: UIButton!
+    @IBAction func Judge(sender: UIButton) {
+        sender.showsTouchWhenHighlighted = true
+        switch sender.currentTitle {
+        case "﹢"? : Method() ; key = 1
+        case "﹣"? : Method() ; key = 2
+        case "×"? : Method() ; key = 3
+        case "÷"? : Method() ; key = 4
+        default : break
+        }
+        if Display.text != "0"{
+            Reset.setTitle("C", forState: .Normal)
+        }else {
+            Reset.setTitle("AC", forState: .Normal)
+        }
     }
+    
+    //判断button为那个运算符
+    
+    func  Method(){if clear {
 
-
+    }else{
+        switch key {
+        case 0 :
+            sum = Int(Display.text!)!
+            clear = true
+        case 1 :
+            sum = sum + Int(Display.text!)!
+            Display.text = "\(sum)"
+            clear = true
+        case 2 :
+            sum = sum - Int(Display.text!)!
+            Display.text = "\(sum)"
+            clear = true
+        case 3 :
+            sum = sum * Int(Display.text!)!
+            Display.text = "\(sum)"
+            clear = true
+        case 4 :
+            sum = sum / Int(Display.text!)!
+            Display.text = "\(sum)"
+            clear = true
+        default : break
+        }
+        }
+        
+    }
+    //获得前一个运算符的运算结果
+    
+    @IBAction func Reroll() {
+        sum = 0
+        Display.text = "0"
+        key = 0
+        Reset.setTitle("AC", forState: .Normal)
+    }
 }
-
